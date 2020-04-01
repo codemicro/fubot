ddb9e94f2bb7fa296e88153c8052ee59bd4543af
Tom <tom@tdpain.net>
2020-04-01 11:14:56 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,11 +4,9 @@ from datetime import date
 client = discord.Client()
 
 channels = [671617756004876328, 624038275245539349]
-#output_channel = 684080752136159244
-
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print('Logged in as {0.user} - may the chaos commence.'.format(client))
 
 @client.event
 async def on_message(message):
@@ -20,8 +18,6 @@ async def on_message(message):
       m = message.content
       a = message.author.mention
       c = message.channel.name
-
-      #channel = client.get_channel(output_channel)
 
       await message.channel.send(f"From {a} in #{c}: {m}")
 
